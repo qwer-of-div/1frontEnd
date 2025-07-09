b699d13a0f4589cf9c8a7efc280e066e2a8e2efa
wzj <794621839@qq.com>
2025-07-09 14:26:33 +0800

    text-scroll

---
+++ pc_vue_vite/src/views/example/text-scroll/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="wrapper">
+    <div class="marquee-wrapper">
+      <div class="marquee-text">
+        这是第一行横向滚动的提示文字信息
+      </div>
+      <div class="marquee-text">
+        这是第一行横向滚动的提示文字信息
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 文字走马灯
+const navList = [
+  { label: "keepAlive", name: 'keepAlive' },
+]
+
+export default {
+  data () {
+    return {
+      page: {
+      },
+      init: {
+        navList
+      }
+    }
+  },
+  created () {
+    console.log(this.$route, this.$route.query)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  left: 30%;
+  transform: translate(-20%, -50%);
+
+  width: 600px;
+  height: 60px;
+  overflow: hidden;
+  border: 4px solid red;
+  border-radius: 5px;
+}
+
+.marquee-wrapper {
+  display: flex;
+  align-items: center;
+  width: 200%;
+  height: 100%;
+  transform: translateX(0);
+  animation: move 6s infinite linear;
+  animation-delay: 3s;
+  /*text-shadow: 1325px 0;*/
+}
+
+.marquee-text {
+  width: 100%;
+  font-size: 20px;
+  white-space: nowrap;
+  /*background-color: red;*/
+}
+
+.marquee-text:nth-child(2) {
+  /* color: red; */
+}
+
+@keyframes move {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(-50%);
+  }
+}
+</style>
--- pc_vue_vite/src/views/menu/code/asyncAwait/index.vue
+++ pc_vue_vite/src/views/menu/code/asyncAwait/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h3>asyncAwait</h3>
     <el-button @click="getList">async/await</el-button>
+    <el-button @click="funcBack">async/await函数返回</el-button>
   </div>
 </template>
 
@@ -25,7 +26,7 @@ export default {
       return new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve(100)
-        }, 1000)
+        }, 500)
       })
     }
 
@@ -43,13 +44,44 @@ export default {
       console.log(result, 'async/await同步')
     }
 
+    const funcBackBefore = async () => {
+      let res = {}
+      try {
+        const result = await list()
+        res = {
+          status: 'success',
+          data: result
+        }
+        console.log(result, result1, 'funcBackBefore-success函数返回')
+      } catch (error) {
+        res = {
+          status: 'error',
+          data: {}
+        }
+        console.log(error, 'funcBackBefore-error函数返回')
+        // throw '不符合的值'
+      }
+      // return res
+    }
+
+    const funcBack = async () => {
+      // 最终返回的结果仍然是按照promise的添加顺序返回的
+      try {
+        const result = await funcBackBefore()
+        console.log(result, 'funcBack函数返回')
+      } catch (error) {
+        console.log(error, 'funcBack-error函数返回')
+      }
+    }
+
     return {
       ...page, // 这样写不好, 里面会失去响应式
       page, // 这样写那么外面就要都基于page来调取, 类型{{page.age}}
       flag,
       count,
       list,
-      getList
+      getList,
+      funcBack
     }
   }
 }
